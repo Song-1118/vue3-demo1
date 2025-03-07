bd750cd6e62795068cb5d1241a8b4d35e6154648
userName <userEmail>
2025-03-07 21:26:41 +0800

    feat : 布局完成

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,66 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="table-box">
+  <!-- 标题 -->
+  <div class="title">
+    <h2>CRUD后台界面</h2>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+  <!-- 功能区 -->
+   <div class="query-box">
+    <el-input
+    v-model="queryInput"
+    style="width: 240px"
+    placeholder="请输入姓名搜索"
+    clearable
+  />
+    <el-button type="primary" @click="queryClick">增加</el-button>
+    <el-table :data="tableData" style="width: 100%" height="250">
+    <el-table-column prop="date" label="日期" width="150" />
+    <el-table-column prop="name" label="姓名" width="120" />
+    <el-table-column prop="state" label="状态" width="120" />
+    <el-table-column prop="city" label="城市" width="120" />
+    <el-table-column fixed="right" label="功能" min-width="120">
+      <template #default>
+        <el-button link type="primary" size="small" @click="delClick">删除</el-button>
+        <el-button link type="primary" size="small" @click="editClick">编辑</el-button>
+      </template>
+    </el-table-column>
+  </el-table>
+   </div>
+  </div>
 </template>
 
+<script lang="ts" setup>
+  import { ref } from 'vue'
+  // 数据
+  let queryInput = ref('')
+
+  const tableData = ref([
+    { date: '2025-03-07', name: '王小虎', state: '已发布', city: '上海' },
+    { date: '2025-03-07', name: '王小虎', state: '已发布', city: '上海' },
+    { date: '2025-03-07', name: '王小虎', state: '已发布', city: '上海' },
+    { date: '2025-03-07', name: '王小虎', state: '已发布', city: '上海' },
+    { date: '2025-03-07', name: '王小虎', state: '已发布', city: '上海' },
+    { date: '2025-03-07', name: '王小虎', state: '已发布', city: '上海' }
+  ])
+
+// 方法
+let delClick = () => {
+  console.log('删除');
+}
+let editClick = () => {
+  console.log("编辑");
+} 
+let queryClick = () => {
+  console.log("增加");
+}
+</script>
+
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+  .table-box {
+    width: 800px;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+  }
 </style>
